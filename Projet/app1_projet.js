90c255f76e84d075ebf2ccdad966229abeeb9921
PMnerea <75248017+PMnerea@users.noreply.github.com>
2020-12-10 13:26:31 +0100

    Add files via upload

--- Projet/app1_projet.js
+++ Projet/app1_projet.js
@@ -82,247 +82,320 @@ function removePerson(mouseEvent) {
 
 
 function ResST() {
-    var Res = { E: 0, H: 0, S: 0, D: 0 };
-    try {
-        var questions = [document.querySelector("input[name='st_1']:checked").value,
-        document.querySelector("input[name='st_2']:checked").value,
-        document.querySelector("input[name='st_3']:checked").value,
-        document.querySelector("input[name='st_4']:checked").value,
-        document.querySelector("input[name='st_5']:checked").value,
-        document.querySelector("input[name='st_6']:checked").value,
-        document.querySelector("input[name='st_7']:checked").value,
-        document.querySelector("input[name='st_8']:checked").value]
+    if (joueurs.length == 0) {
+        alert('Veuillez vous enregistrer dans la liste de joueurs.');
     }
-    catch (err) {
-        alert("Toutes les réponses n'ont pas été cochées.");
-    }
-    for (var i = 0; i < questions.length; i++) {
-        if (i == 1 || i == 6) {
-            switch (questions[i]) {
-                case 'eleven':
-                    Res.E++;
-                    break;
-                default:
-                    Res.H++;
-                    Res.S++;
-                    Res.D++;
+    else {
+        let ps = prompt('Veuillez entrer votre pseudo :');
+
+        let present = false;
+        for (let i = 0; i < joueurs.length; i++) {
+            if (ps == joueurs[i].pseudo) {
+                present = true;
+                break;
             }
         }
-        else if (i == 5) {
-            switch (questions[i]) {
-                case 'hopper':
-                    Res.H++;
-                    Res.S++;
-                    break;
-                default:
-                    Res.E++;
-                    Res.D++;
+
+        if (present) {
+            var Res = { E: 0, H: 0, S: 0, D: 0 };
+            try {
+                var questions = [document.querySelector("input[name='st_1']:checked").value,
+                document.querySelector("input[name='st_2']:checked").value,
+                document.querySelector("input[name='st_3']:checked").value,
+                document.querySelector("input[name='st_4']:checked").value,
+                document.querySelector("input[name='st_5']:checked").value,
+                document.querySelector("input[name='st_6']:checked").value,
+                document.querySelector("input[name='st_7']:checked").value,
+                document.querySelector("input[name='st_8']:checked").value]
             }
+            catch (err) {
+                alert("Toutes les réponses n'ont pas été cochées.");
+            }
+            for (var i = 0; i < questions.length; i++) {
+                if (i == 1 || i == 6) {
+                    switch (questions[i]) {
+                        case 'eleven':
+                            Res.E++;
+                            break;
+                        default:
+                            Res.H++;
+                            Res.S++;
+                            Res.D++;
+                    }
+                }
+                else if (i == 5) {
+                    switch (questions[i]) {
+                        case 'hopper':
+                            Res.H++;
+                            Res.S++;
+                            break;
+                        default:
+                            Res.E++;
+                            Res.D++;
+                    }
+                }
+                else {
+                    switch (questions[i]) {
+                        case 'eleven':
+                            Res.E++;
+                            break;
+                        case 'hopper':
+                            Res.H++;
+                            break;
+                        case 'steve':
+                            Res.S++;
+                            break;
+                        default:
+                            Res.D++;
+                    }
+                }
+            }
+
+            var max = 0
+            var pers;
+            for (let i in Res) {
+                if (Res[i] > max) {
+                    max = Res[i];
+                    pers = i;
+                }
+            }
+            let perso;
+
+            const hparagraphe = document.createElement('h3');
+            const paragraphe = document.createElement('p');
+            const image = document.createElement('img');
+            const affich = document.getElementById("pSt");
+            image.setAttribute("width", 400);
+            affich.innerHTML = '';
+            if (pers == 'E') {
+                perso = "Eleven";
+                hparagraphe.innerText = "Eleven";
+                paragraphe.innerText = "Oulala, t'as des problèmes de colère, il faut parfois prendre les évènement avec plus de recul... Mais sinon ça va, tu es une bonne personne qui donnerait tout pour ses amis et sa famille. Ps = Abuse pas trop sur les gauffres";
+                image.setAttribute("src", "Images/eleven.png");
+            }
+            else if (pers == 'H') {
+                perso = "Jim Hopper";
+                hparagraphe.innerText = "Jim Hopper"; 
+                paragraphe.innerText = "Tu es courageux et déterminé. Ton rôle de sherif te correspond parfaitement, tu cherches toujours à protéger les autres.";
+                image.setAttribute("src", "Images/hopper.png");
+            }
+            else if (pers == 'D') {
+                perso = "Dustin Henderson";
+                hparagraphe.innerText = 'Dustin Henderson';
+                paragraphe.innerText = "Tu es le rigolo du groupe. Toujours de bonne humeur, tu sais redonner le sourire à tes amis. Il faut juste que tu ai un peu plus confiance en toi!";
+                image.setAttribute("src", "Images/dustin.png");
+            }
+            else {
+                perso = "Steve Harrington";
+                hparagraphe.innerText = 'Steve Harrington';
+                paragraphe.innerText = "Tu as su grandir grâce aux aventures que tu as pu vivre. T'es le gars sympa du groupe, tes amis t'apprécient beaucoup";
+                image.setAttribute("src", "Images/steve.png");
+            }
+            affich.appendChild(hparagraphe);
+            affich.appendChild(image);
+            affich.appendChild(paragraphe);
+
+            const ID = (ps).concat('_', 'st');
+            const caseRes = document.getElementById(ID);
+
+            caseRes.innerText = perso;
+
         }
         else {
-            switch (questions[i]) {
-                case 'eleven':
-                    Res.E++;
-                    break;
-                case 'hopper':
-                    Res.H++;
-                    break;
-                case 'steve':
-                    Res.S++;
-                    break;
-                default:
-                    Res.D++;
-            }
+            alert('Ce pseudo n\'est pas valide');
         }
-    }
-
-    var max = 0
-    var pers;
-    for (let i in Res) {
-        if (Res[i] > max) {
-            max = Res[i];
-            pers = i;
-        }
-    }
-    let perso;
-    if (pers == 'E') {
-        perso = 'Eleven';
-    }
-    else if (pers == 'H') {
-        perso = 'Jim Hopper';
-    }
-    else if (pers == 'D') {
-        perso = 'Dustin Henderson';
-    }
-    else {
-        perso = 'Steve Harrington';
-    }
-
-    let ps = prompt('Veuillez entrer votre pseudo :');
-
-    let present = false;
-    for (let i = 0; i < joueurs.length; i++) {
-        if (ps == joueurs[i].pseudo) {
-            present = true;
-            break;
-        }
-    }
-
-    const ID = (ps).concat('_', 'st');
-    const caseRes = document.getElementById(ID);
-
-    if (!present) {
-        alert('Ce pseudo n\'est pas valide');
-    }
-    else {
-        caseRes.innerText = perso;
     }
 }
 
 function ResLCDP() {
-    var Res = { T: 0, P: 0, H: 0, B: 0 };
-    try {
-        var questions = [document.querySelector("input[name='lcdp_1']:checked").value,
-        document.querySelector("input[name='lcdp_2']:checked").value,
-        document.querySelector("input[name='lcdp_3']:checked").value,
-        document.querySelector("input[name='lcdp_4']:checked").value,
-        document.querySelector("input[name='lcdp_5']:checked").value,
-        document.querySelector("input[name='lcdp_6']:checked").value,
-        document.querySelector("input[name='lcdp_7']:checked").value]
-    }
-    catch (err) {
-        alert("Toutes les réponses n'ont pas été cochées.");
-    }
-    for (var i = 0; i < questions.length; i++) {
-        switch (questions[i]) {
-            case 'professeur':
-                Res.P++;
-                break;
-            case 'tokyo':
-                Res.T++;
-                break;
-            case 'helsinki':
-                Res.H++;
-                break;
-            default:
-                Res.B++;
-        }
-    }
-
-    let max = 0
-    let pers;
-    for (let i in Res) {
-        if (Res[i] > max) {
-            max = Res[i];
-            pers = i;
-        }
-    }
-
-    let perso;
-
-    const paragraphe = document.createElement('h3'); 
-    const affich = document.getElementById("pLcdp");
-    if (pers == 'T') {
-        perso = 'Tokyo';  
-        paragraphe.innerText = "Tokyo"; 
-    }
-    else if (pers == 'P') {
-        perso = 'Le professeur';
-        paragraphe.innerText = "Le professeur"; 
-    }
-    else if (pers == 'H') {
-        perso = 'Helsinki';
-        paragraphe.innerText = "Helsinki"; 
+    if (joueurs.length == 0) {
+        alert('Veuillez vous enregistrer dans la liste de joueurs.');
     }
     else {
-        perso = 'Berlin';
-        paragraphe.innerText = "Berlin"; 
-    }
-    affich.appendChild(paragraphe);
+        let ps = prompt('Veuillez entrer votre pseudo :');
 
-    let ps = prompt('Veuillez entrer votre pseudo :');
-
-    let present = false;
-    for (let i = 0; i < joueurs.length; i++) {
-        if (ps == joueurs[i].pseudo) {
-            present = true;
-            break;
+        let present = false;
+        for (let i = 0; i < joueurs.length; i++) {
+            if (ps == joueurs[i].pseudo) {
+                present = true;
+                break;
+            }
         }
-    }
 
-    const ID = (ps).concat('_', 'lcdp');
-    const caseRes = document.getElementById(ID);
+        if (present) {
+            var Res = { T: 0, P: 0, H: 0, B: 0 };
+            try {
+                var questions = [document.querySelector("input[name='lcdp_1']:checked").value,
+                document.querySelector("input[name='lcdp_2']:checked").value,
+                document.querySelector("input[name='lcdp_3']:checked").value,
+                document.querySelector("input[name='lcdp_4']:checked").value,
+                document.querySelector("input[name='lcdp_5']:checked").value,
+                document.querySelector("input[name='lcdp_6']:checked").value,
+                document.querySelector("input[name='lcdp_7']:checked").value]
+            }
+            catch (err) {
+                alert("Toutes les réponses n'ont pas été cochées.");
+            }
+            for (var i = 0; i < questions.length; i++) {
+                switch (questions[i]) {
+                    case 'professeur':
+                        Res.P++;
+                        break;
+                    case 'tokyo':
+                        Res.T++;
+                        break;
+                    case 'helsinki':
+                        Res.H++;
+                        break;
+                    default:
+                        Res.B++;
+                }
+            }
 
-    if (!present) {
-        alert('Ce pseudo n\'est pas valide');
-    }
-    else {
-        caseRes.innerText = perso;
+            let max = 0
+            let pers;
+            for (let i in Res) {
+                if (Res[i] > max) {
+                    max = Res[i];
+                    pers = i;
+                }
+            }
+
+            const hparagraphe = document.createElement('h3');
+            const paragraphe = document.createElement('p');
+            const image = document.createElement('img');
+            const affich = document.getElementById("pLcdp");
+            image.setAttribute("width", 400);
+            affich.innerHTML = '';
+
+            if (pers == 'T') {
+                perso = "Tokyo";
+                hparagraphe.innerText = "Tokyo";
+                paragraphe.innerText = "Tu es fort, intrépide et sûr de toi mais quand on s'en prend à une personne que tu aimes, tu ne te contrôle plus et tu fais tout pour les venger, tu es aussi très humble et gentil malgré tes allures de rebelle.";
+                image.setAttribute("src", "Images/tokyo.png");
+            }
+            else if (pers == 'P') {
+                perso = "Le professeur";
+                hparagraphe.innerText = "Le professeur";
+                paragraphe.innerText = "Tu réfléchis avant d'agir, prends ton temps et tu n'es pas brusque, ce que tu entreprends est sûr d'être réussi, tu serais un parfait professeur!";
+                image.setAttribute("src", "Images/profesor.png");
+            }
+            else if (pers == 'H') {
+                perso = "Helsinki";
+                hparagraphe.innerText = "Helsinki";
+                paragraphe.innerText = 'Pour toi un plan doit être violent mais pas que! Tu as aussi un grand coeur et tu n\'hésite pas à aider tes proches quand ils en ont besoin';
+                image.setAttribute("src", "Images/helsinki.png");
+            }
+            else {
+                perso = "Berlin";
+                hparagraphe.innerText = "Berlin";
+                paragraphe.innerText = "Tu aimes le pouvoir et tu n'as peur de rien, tu est souvent pessimiste comme Berlin, tu es casanier et très solidaire, personne ne peut t'empêcher de terminer ce que tu as commencé!";
+                image.setAttribute("src", "Images/berlin.png");
+            }
+            affich.appendChild(hparagraphe);
+            affich.appendChild(image);
+            affich.appendChild(paragraphe);
+
+            const ID = (ps).concat('_', 'lcdp');
+            const caseRes = document.getElementById(ID);
+
+            caseRes.innerText = perso;
+        }
+        else {
+            alert('Ce pseudo n\'est pas valide');
+        }
     }
 }
 
 function ResMini() {
-    let Res = { B: 0, S: 0, K: 0 };
-    try {
-        let questions = [document.querySelector("input[name='min_1']:checked").value,
-        document.querySelector("input[name='min_2']:checked").value,
-        document.querySelector("input[name='min_3']:checked").value,
-        document.querySelector("input[name='min_4']:checked").value,
-        document.querySelector("input[name='min_5']:checked").value,
-        document.querySelector("input[name='min_6']:checked").value]
-    }
-    catch (err) {
-        alert("Toutes les réponses n'ont pas été cochées.");
-    }
-    for (let i = 0; i < questions.length; i++) {
-        switch (questions[i]) {
-            case 'bob':
-                Res.B++;
-                break;
-            case 'stuart':
-                Res.S++;
-                break;
-            default:
-                Res.K++;
-        }
-    }
-
-    let max = 0
-    let pers;
-    for (let i in Res) {
-        if (Res[i] > max) {
-            max = Res[i];
-            pers = i;
-        }
-    }
-    let perso;
-    if (pers == 'B') {
-        perso = 'Bob';
-    }
-    else if (pers == 'S') {
-        perso = 'Stuart';
+    if (joueurs.length == 0) {
+        alert("Veuillez vous enregistrer dans la liste de joueurs.");
     }
     else {
-        perso = 'Kevin';
-    }
+        let ps = prompt('Veuillez entrer votre pseudo :');
 
-    let ps = prompt('Veuillez entrer votre pseudo :');
+        let present = false;
+        for (let i = 0; i < joueurs.length; i++) {
+            if (ps == joueurs[i].pseudo) {
+                present = true;
+                break;
+            }
+        }
 
-    let present = false;
-    for (let i = 0; i < joueurs.length; i++) {
-        if (ps == joueurs[i].pseudo) {
-            present = true;
-            break;
+        if (present) {
+
+            let Res = { B: 0, S: 0, K: 0 };
+            try {
+                var questions = [document.querySelector("input[name='min_1']:checked").value,
+                document.querySelector("input[name='min_2']:checked").value,
+                document.querySelector("input[name='min_3']:checked").value,
+                document.querySelector("input[name='min_4']:checked").value,
+                document.querySelector("input[name='min_5']:checked").value,
+                document.querySelector("input[name='min_6']:checked").value]
+            }
+            catch (err) {
+                alert("Toutes les réponses n'ont pas été cochées.");
+            }
+            for (let i = 0; i < questions.length; i++) {
+                switch (questions[i]) {
+                    case 'bob':
+                        Res.B++;
+                        break;
+                    case 'stuart':
+                        Res.S++;
+                        break;
+                    default:
+                        Res.K++;
+                }
+            }
+
+            let max = 0
+            let pers;
+            for (let i in Res) {
+                if (Res[i] > max) {
+                    max = Res[i];
+                    pers = i;
+                }
+            }
+
+            const hparagraphe = document.createElement('h3');
+            const paragraphe = document.createElement('p');
+            const image = document.createElement('img');
+            const affich = document.getElementById("pMini");
+            image.setAttribute("width", 400);
+            affich.innerHTML = '';
+
+            if (pers == 'B') {
+                perso = "Bob";
+                hparagraphe.innerText = "Bob";
+                paragraphe.innerText = "Tu aimes t'occuper de la tribu et de ton doudou. Des \"bananas\" aussi! Tu es bien mignon mais aussi bien plus démoniaque quand tu le veux!";
+                image.setAttribute("src", "Images/bob.png");
+            }
+            else if (pers == 'S') {
+                perso = "Stuart";
+                hparagraphe.innerText = "Stuart";
+                paragraphe.innerText = "Tu aimes beaucoup la musique, en particulier la guitare. Mais ce que tu préfères, c'est la \"banana\"! Bien sûr, si on a besoin de toi, tu pourras toujours répondre \"présent\"!";
+                image.setAttribute("src", "Images/stuart.png");
+            }
+            else {
+                perso = "Kevin";
+                hparagraphe.innerText = "Kevin";
+                paragraphe.innerText = "Petit intello, va! Mais aussi grand rigolo! Sans toi, la saga \"Minions\" n'existerait pas sans doute. Je me trompe? Ah j'oubliais! Tu es officiellement fan des \"bananas\".";
+                image.setAttribute("src", "Images/kevin.png");
+            }
+
+            affich.appendChild(hparagraphe);
+            affich.appendChild(image);
+            affich.appendChild(paragraphe);
+
+            const ID = (ps).concat('_', 'mini');
+            const caseRes = document.getElementById(ID);
+
+            caseRes.innerText = perso;
+        }
+        else {
+            alert('Ce pseudo n\'est pas valide');
         }
     }
 
-    const ID = (ps).concat('_', 'mini');
-    const caseRes = document.getElementById(ID);
-
-    if (!present) {
-        alert('Ce pseudo n\'est pas valide');
-    }
-    else {
-        caseRes.innerText = perso;
-    }
 }
